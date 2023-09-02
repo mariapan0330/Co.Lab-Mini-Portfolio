3a6a623fcd8e74cb589375a8a71a0512d399541d
Maria Panagos <maria.pan0330@gmail.com>
2023-09-02 18:42:24 -0500

    responsive nav bar

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 export const Landing: React.FC = () => {
   return (
-    <div className="h-screen mx-auto flex flex-col justify-center items-center px-8 lg:px-0" id="landing">
-      <div className="text-3xl font-bold lg:text-7xl">Results-driven full stack developer</div>
-      <div className="text-lg lg:text-2xl py-10">with a strong foundation in psychology and statistics, transitioning into the field fo software development</div>
-      <div className="text-xl lg:text-2xl font-bold text-white">
+    <div className="h-screen mx-auto flex flex-col justify-center items-center px-10 md:px-20" id="landing">
+      <div className="text-5xl font-bold md:text-7xl">Results-driven full stack developer</div>
+      <div className="text-lg md:text-2xl py-10">with a strong foundation in psychology and statistics, transitioning into the field of software development</div>
+      <div className="text-xl md:text-2xl font-bold text-white">
         <a target="_blank" rel='noreferrer' href={require("../assets/Maria Panagos Resume for CoLab.pdf")} className="w-30 h-16 lg:w-48 lg:h-20 bg-black px-10 py-3 flex items-center justify-center">Resume</a>
       </div>
-      <img src={require('../assets/landing_images.png')} alt="My hobbies and me" className="hidden md:flex lg:h-1/5 lg:w-auto my-20" />
+      <img src={require('../assets/landing_images.png')} alt="My hobbies and me" className="hidden md:flex md:w-fit lg:h-1/5 my-20" />
     </div>
   );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,37 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { HashLink } from "react-router-hash-link";
 
+type LinksInfo = {
+  name: string;
+  link: string;
+};
+
 export const NavBar: React.FC = () => {
+  let links: LinksInfo[] = [
+    { name: "Bio", link: "#bio" },
+    { name: "Projects", link: "#projects" },
+    { name: "Fancy a Riddle?", link: "#riddle" },
+  ];
+
+  let [open, setOpen] = useState(false);
+
   return (
-    <div className="h-20 bg-white drop-shadow-xl sticky top-0 flex justify-evenly items-center">
-      {/* ITEM 1 & 2: LOGO & TITLE */}
-      <HashLink to="#top" className="text-3xl font-bold flex">
-        <img
-          src={require("../assets/m icon.png")}
-          className="h-10 px-3"
-          alt="logo"
-        />
-        <div>Maria Panagos</div>
-      </HashLink>
+    <div className="h-20 bg-white drop-shadow-xl sticky top-0 z-auto px-0 md:px-3 lg:px-40">
+      <div className="flex">
+        {/* ITEM 1 & 2: LOGO & TITLE */}
+        <HashLink
+          to="#top"
+          className="text-3xl font-bold flex items-center w-full py-4 md:pb-2 md:pt-0"
+        >
+          <img
+            src={require("../assets/m icon.png")}
+            className="h-10 px-3"
+            alt="logo"
+          />
+          <div>Maria Panagos</div>
+        </HashLink>
 
-      {/* ITEM 3: BIO HASHLINK */}
-      <HashLink to="#bio" className="text-xl">
-        <div>Bio</div>
-      </HashLink>
+        {/* SMALL SCREENS: menu button */}
+        <div
+          onClick={() => setOpen((prev) => !prev)}
+          className="text-3xl absolute right-8 top-5 md:hidden"
+        >
+          <img src={require("../assets/Menu Btn.png")} className="w-8" alt="" />
+        </div>
 
-      {/* ITEM 3: PROJECTS HASHLINK */}
-      <HashLink to="#projects" className="text-xl">
-        <div>Projects</div>
-      </HashLink>
+        {/* ITEMS 3,4,5: Bio, Projects, Riddle */}
+        <ul
+          className={`z-[-1] w-full absolute md:static md:flex md:items-center md:justify-evenly transition-all duration-200 ease-in ${
+            open ? "top-20" : "top-[-295px]"
+          }`}
+        >
+          {links.map((link, i) => (
+            <li
+              key={`navlink-${i}`}
+              className="text-xl h-20 pt-5 bg-gray-800 text-white md:text-black md:bg-white"
+            >
+              <HashLink
+                to={link.link}
+                className="lg:hover:text-gray-400 cursor-pointer duration-200"
+              >
+                {link.name}
+              </HashLink>
+            </li>
+          ))}
 
-      {/* ITEM 3: RIDDLE HASHLINK */}
-      <HashLink to="#riddle" className="text-xl">
-        <div>Fancy a Riddle?</div>
-      </HashLink>
-
-      {/* ITEM 3: RESUME IN NEW TAB */}
-      <div className="text-xl text-white">
-        <a target='_blank' rel='noreferrer' href={require("../assets/Maria Panagos Resume for CoLab.pdf")} className="bg-black px-10 py-3 flex items-center jutify-center font-bold">Resume</a>
+          {/* ITEM 3: RESUME IN NEW TAB */}
+          <div className="text-xl text-white">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={require("../assets/Maria Panagos Resume for CoLab.pdf")}
+              className="bg-black px-10 py-3 flex items-center justify-center font-bold"
+            >
+              Resume
+            </a>
+          </div>
+        </ul>
       </div>
     </div>
   );
