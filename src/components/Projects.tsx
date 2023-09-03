4fa107b4e7984cc36ae671fd0319085ffb24aae0
Maria Panagos <maria.pan0330@gmail.com>
2023-09-03 15:30:40 -0500

    one page wide projects carousel

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useMediaQuery } from "react-responsive";
 
-type ProjectCard = {
+type ProjectCardDetails = {
   image?: string;
   title: string;
   live?: string;
@@ -10,14 +10,16 @@ type ProjectCard = {
 };
 
 interface ProjectCardProps {
-  card: ProjectCard;
-  position: number;
-  z: number;
-  i: number;
+  cards?: ProjectCardDetails[];
+  card: ProjectCardDetails;
+  position?: number;
+  z?: number;
+  i?: number;
 }
 
 export const Projects: React.FC = () => {
-  let cards: ProjectCard[] = [
+  const [currentCard, setCurrentCard] = useState<number>(0);
+  let cards: ProjectCardDetails[] = [
     {
       image: "personal website",
       title: "This site!",
@@ -67,77 +69,93 @@ export const Projects: React.FC = () => {
       repo: "https://github.com/mariapan0330/Phonebook",
     },
   ];
+
+  const nextCard = (direction: string) => {
+    direction === "forward" && setCurrentCard((prev) => (prev + 1) % 6);
+    direction === "backward" && setCurrentCard((prev) => (prev - 1+6) % 6);
+  };
+
   return (
-    <div className="h-content md:h-[50rem] pb-40 mx-2 md:mx-80 relative">
+    <div className="h-content md:h-[50rem] w-full pb-40 mx-2 relative">
       <div className="text-3xl font-bold md:text-5xl">Projects</div>
-      <div className="md:flex md:flex-wrap mt-20">
-        {cards.map((card, i) => (
-          <div key={`project-card-${i}`} className="select-none">
-            <ProjectCard
-              card={card}
-              position={i * 12}
-              z={(10 - i) * 10}
-              i={i}
-            />
-          </div>
-        ))}
+      <div className="select-none flex justify-center mt-20 cursor-pointer text-3xl font-bold">
+        <span
+          onClick={() => nextCard("backward")}
+          className="mx-16"
+        >{`<<<`}</span>
+        <span
+          onClick={() => nextCard("forward")}
+          className="mx-16"
+        >{`>>>`}</span>
+      </div>
+      <div className="md:flex md:flex-wrap justify-center">
+        {/* {cards.map((card, i) => ( */}
+        <div className="select-none">
+          {/* <div key={`project-card-${i}`} className="select-none"> */}
+          <ProjectCard
+            // cards={cards}
+            card={cards[currentCard]}
+            // position={i * 12}
+            // z={(10 - i) * 10}
+            // i={i}
+          />
+        </div>
+        {/* ))} */}
       </div>
     </div>
   );
 };
 
-const ProjectCard: React.FC<ProjectCardProps> = ({
-  card,
-  position,
-  z,
-  i,
-}) => {
-  const [clicked, setClicked] = useState(true);
+const ProjectCard: React.FC<ProjectCardProps> = ({ cards, card, i }) => {
+  // TRY NPM'S REACT-STACK-CARDS (https://www.npmjs.com/package/@webileapps/react-stack-cards)
+  // DEMO: https://yoloten.github.io/react-stack-cards/
+  const [showCard, setShowCard] = useState(true);
   const isPhone = useMediaQuery({ query: "(max-width: 1000px)" });
   let colors = ["#FFB2E0", "#72CDAA", "#4959FE", "#61BDFD", "#FECC38"];
+
   return (
-    <div
-      className={`z-${z} ${
-        !isPhone && `absolute ${clicked ? "right" : "left"}-${position}`
-      } flex flex-col bg-white p-4 m-4 w-80 min-h-[470px] max-h-full shadow-md transition-all duration-500`}
-      onClick={() => {
-        !isPhone && setClicked((prev) => !prev);
-      }}
-    >
-      <img
-        src={require(`../assets/projects/${card.image}.png`)}
-        alt={card.title}
-      />
-      <div className="flex-none text-xl font-bold py-4">{card.title}</div>
-      <div className="flex-row grow text-md">{card.description}</div>
-      <div className="flex-none links my-6 font-bold">
-        {card.live && (
-          <a
-            href={card.live}
-            target="_blank"
-            rel="noreferrer"
-            className="text-white py-2 px-4 border-solid border-4 rounded-sm border-white hover:border-blue-800"
-            style={{
-              backgroundColor: `${colors[Math.floor(Math.random() * 5)]}`,
-            }}
-          >
-            Live
-          </a>
-        )}
-        {card.live && card.repo && <span className="px-3"></span>}
-        {card.repo && (
-          <a
-            href={card.repo}
-            target="_blank"
-            rel="noreferrer"
-            className="text-white py-2 px-4 border-solid border-4 rounded-sm border-white hover:border-blue-800"
-            style={{
-              backgroundColor: `${colors[Math.floor(Math.random() * 5)]}`,
-            }}
-          >
-            Repo
-          </a>
-        )}
+    <div className={`${showCard ? "flex" : "hidden"}`}>
+      <div
+        className={`md:absolute md:top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 flex flex-col m-auto my-3 md:m-0 bg-white p-4 w-80 min-h-[470px] max-h-full shadow-md transition-all duration-500`}
+        onClick={() => {
+          !isPhone && setShowCard((prev) => !prev);
+        }}
+      >
+        <img
+          src={require(`../assets/projects/${card.image}.png`)}
+          alt={card.title}
+        />
+        <div className="flex-none text-xl font-bold py-4">{card.title}</div>
+        <div className="flex-row grow text-md">{card.description}</div>
+        <div className="flex-none links my-6 font-bold">
+          {card.live && (
+            <a
+              href={card.live}
+              target="_blank"
+              rel="noreferrer"
+              className="text-white py-2 px-4 border-solid border-4 rounded-sm border-white hover:border-blue-800"
+              style={{
+                backgroundColor: `${colors[Math.floor(Math.random() * 5)]}`,
+              }}
+            >
+              Live
+            </a>
+          )}
+          {card.live && card.repo && <span className="px-3"></span>}
+          {card.repo && (
+            <a
+              href={card.repo}
+              target="_blank"
+              rel="noreferrer"
+              className="text-white py-2 px-4 border-solid border-4 rounded-sm border-white hover:border-blue-800"
+              style={{
+                backgroundColor: `${colors[Math.floor(Math.random() * 5)]}`,
+              }}
+            >
+              Repo
+            </a>
+          )}
+        </div>
       </div>
     </div>
   );
